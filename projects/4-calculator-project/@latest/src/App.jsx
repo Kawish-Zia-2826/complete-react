2001b27ab88ac929699c9e8d9b19b53e2f9fb19a
Kawish Zia <kawishzia2826@gmail.com>
2024-12-06 20:16:47 -0800

    update

--- projects/4-calculator-project/@latest/src/App.jsx
+++ projects/4-calculator-project/@latest/src/App.jsx
@@ -1,4 +1,5 @@
 import style from  './App.module.css'
+import ButtonContainer from './components/ButttonContainer'
 import Display from './components/Display'
 
 function App() {
@@ -7,12 +8,8 @@ function App() {
   return (
     <div className={style.calculator} >
     <Display></Display>
-      <div className={style.buttonsContainer}>
-    <button className={style.button}>c</button>
-    <button className={style.button}>1</button>
-    <button className={style.button}>2</button>
-    <button className={style.button}>+</button>
-      </div>
+    <ButtonContainer></ButtonContainer>
+   
     </div>
   )
 }
--- projects/4-calculator-project/@latest/src/components/Display.jsx
+++ projects/4-calculator-project/@latest/src/components/Display.jsx
@@ -1,6 +1,7 @@
 import style from './Display.module.css'
 const Display = ()=>{
-return(  <input className={style.display} type="text" />
+return(
+    <input className={style.display} type="text" />
 
 );
 }
